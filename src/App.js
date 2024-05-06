0347dde37e63c6790969b0a2a7feb0f33b255a46
pavi2306 <pavipavithra23102001@gmail.com>
2024-05-06 23:46:35 +0530

    changes added new 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import "./App.css";
-import { Route, Router, Routes } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import Preloader from "../src/components/Pre";
-import { useEffect, useState } from "react";
-import { Navbar } from "react-bootstrap";
-import ScrollToTop from "./components/ScrollToTop";
+import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
+import Resume from "./components/Resume/ResumeNew";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
+import "./style.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [load, upadateLoad] = useState(true);
@@ -17,6 +27,7 @@ function App() {
 
     return () => clearTimeout(timer);
   }, []);
+
   return (
     <Router>
       <Preloader load={load} />
@@ -25,10 +36,10 @@ function App() {
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/project" element={<Projects />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="*" element={<Navigate to="/" />} /> */}
+          <Route path="/project" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
       </div>
