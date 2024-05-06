084d6da8035ffec98dffd9140ee4a1e05eece8b6
pavi2306 <pavipavithra23102001@gmail.com>
2024-05-06 16:14:45 +0530

    new changes added

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Pavithra </span>
+            from <span className="purple"> Pondicherry, India.</span>
+            <br />
+            I am a Fresher.
+            <br />
+            I have completed
+            <br />
+            <br />
+            BTech Information Technology.
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Reading Tech Blogs
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
