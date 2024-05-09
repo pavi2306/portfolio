0e81830e7948f5f6e5f0ae8c795f7b69c1cedfca
pavi2306 <pavipavithra23102001@gmail.com>
2024-05-09 22:34:02 +0530

    changes added site

---
+++ src/Data/PersonalData.js
@@ -0,0 +1,14 @@
+const PersonalData = {
+  firstName: "Pavithra",
+  lastName: "Ramesh",
+  nickName: "",
+  mobNo: "",
+  email: "pavipavithra23102001@gmail.com",
+  address: "India",
+  aboutMe:
+    "Hi Everyone, I am Pavithra from Pondicherry, India. I learned Html & Css, Bootstrap, JavaScript amd React.",
+  resumeLink: "",
+  imageUrl: "",
+};
+export const autoTypeData = ["FrontEnd Developer", "Fresher"];
+export default PersonalData;
