ac46fd3cebefef77cf2b1fb4360b4f4b02782d95
pavi2306 <pavipavithra23102001@gmail.com>
2024-05-09 23:32:17 +0530

    changes added

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter, Router, Route } from "react-router-dom";
 import store from "./store/theme";
-import Home from "./components/Home/Home";
+import { Home } from "./components/Home/Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
-      <Router>
+      <Home />
+      {/* <Router>
         <Route path="/" component={<Home />} />
-      </Router>
+      </Router> */}
     </BrowserRouter>
   </Provider>
 );
