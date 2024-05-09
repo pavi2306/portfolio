8b6decd467d4411b7485d261b45faea743408126
pavi2306 <pavipavithra23102001@gmail.com>
2024-05-10 00:52:41 +0530

    changes added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useSelector } from "react-redux";
+import "./App.css";
+import { Home } from "./components/Home/Home";
+import { Navbar } from "./components/Navbar/Navbar";
 function App() {
+  const theme = useSelector((state) => state.theme);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={theme}>
+      <Navbar />
+      <Home />
+      {/* <Education />
+      <Projects />
+      <AboutMe />
+      <Footer />
+      <Mode /> */}
     </div>
   );
 }
